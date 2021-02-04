de360fef309cc1f1372ef55b30e9198ffdcf66e9
Alciomar Hollanda <alciomarhollanda@gmail.com>
2021-02-04 12:03:41 -0200

    Salvando links entre página de login e cadastro

--- reg.php
+++ reg.php
@@ -69,6 +69,14 @@ if(isset($_POST['sub'])){
                                
                     </td>
                 </tr>
+
+
+
+                <tr>
+                    <td>
+                        <a href="login.php"> Login</a>
+                    </td>
+                </tr>
             </table>
     </body>
 </html>
