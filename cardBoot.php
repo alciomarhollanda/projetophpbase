37eabe156c7c08cd5230e4d3dfc41708b3b50468
Alciomar Hollanda <alciomarhollanda@gmail.com>
2021-12-17 08:53:31 -0300

    Add

---
+++ cardBoot.php
@@ -0,0 +1,49 @@
+<?php
+    include 'connect.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <div class='row'>
+<?php 
+
+    $sq="select * from reg";
+    $qu=mysqli_query($con,$sq);
+    while($f= mysqli_fetch_assoc($qu)){
+    
+?> 
+        <div class='col-sm-6'>
+            <div class='card'>
+            <div class='card-body'>
+                <h5 class='card-title'><?php echo $f['name']; ?> </h5>
+                <p class='card-text'>With supporting text below as a natural lead-in to additional content.</p>
+                <a href='#' class='btn btn-primary'><?php echo $f['username']; ?></a>
+            </div>
+            </div>
+        </div>
+<?php      
+    }    
+?>
+        
+    </div>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
